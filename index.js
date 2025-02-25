3dd294e4e4b310f7ee5c1684d0a54d073b0fb1e9
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2025-02-25 10:06:59 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const express = require("express");
-const app = express();
+const app = require("./src/app");
+
 require("dotenv").config;
 const port = process.env.port || 5000;
-
-app.get("/", (req, res) => {
-  res.send(`Abryonix Server is Running on Port: ${port}`);
-});
 
 app.listen(port, () => {
   console.log(`Abryonix Server is Running on : ${port}`);
---
+++ src/app.js
@@ -0,0 +1,20 @@
+// definition
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("./middleware/logger");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(logger);
+
+// playground
+
+//
+
+app.get("/", (req, res) => {
+  res.send(`Abryonix Server is Running`);
+});
+
+module.exports = app;
